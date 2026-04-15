bd5dc6b329b576b9de858f60763bce9e299d827e
SantiG95 <santiguerra86@gmail.com>
2026-04-15 00:37:23 -0300

    Limpieza de archivos

--- Metodos/RectanguloMedioCompuesto.py
+++ Metodos/RectanguloMedioCompuesto.py
@@ -6,3 +6,17 @@ def rectanguloMedioCompuesto(funcion, inicio, fin, n):
 
     integral = h * np.sum(funcion(x_medio))
     return integral
+
+def rectanguloIzquierdoCompuesto(funcion, inicio, fin, n):
+    h = (fin - inicio) / n
+    # Los x van desde 'inicio' hasta el penúltimo punto (no toca 'fin')
+    x_izq = np.linspace(inicio, fin - h, n)
+    integral = h * np.sum(funcion(x_izq))
+    return integral
+
+def rectanguloDerechoCompuesto(funcion, inicio, fin, n):
+    h = (fin - inicio) / n
+    # Los x van desde el segundo punto hasta 'fin' (no toca 'inicio')
+    x_der = np.linspace(inicio + h, fin, n)
+    integral = h * np.sum(funcion(x_der))
+    return integral
--- pruebaMontecarlo.py
+++
@@ -1,5 +0,0 @@
-from Metodos.MetodoMonteCarlo import metodoMonteCarloIntegral
-
-f = lambda x: x**2
-res = metodoMonteCarloIntegral(f, (0, 1), 10000, actual_value=1/3, seed=123)
-print(res)
